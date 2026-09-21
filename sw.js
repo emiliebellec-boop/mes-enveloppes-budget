4944194989f056bf2f3464b3e141f02da4da3ff0
emiliebellec-boop <emilie.bellec@gmail.com>
2026-09-22 01:02:02 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="mes-enveloppes-v21";
+const CACHE="mes-enveloppes-v22";
 const ASSETS=["./","./index.html","./manifest.json","./apple-touch-icon.png","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
